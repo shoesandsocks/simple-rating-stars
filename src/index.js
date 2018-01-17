a51487aa8c06f1b698bb7b0289af24a480f62b48
RL Brown <rlbrown72@gmail.com>
2018-01-17 12:07:01 -0500

    polyfill fill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
+/* eslint-disable no-bitwise, prefer-rest-params, no-extend-native, no-plusplus */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 const intRegex = /^\d+$/;
+
+if (!Array.prototype.fill) {
+  Object.defineProperty(Array.prototype, 'fill', {
+    value(value) {
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+      const O = Object(this);
+      const len = O.length >>> 0;
+      const start = arguments[1];
+      const relativeStart = start >> 0;
+      let k =
+        relativeStart < 0
+          ? Math.max(len + relativeStart, 0)
+          : Math.min(relativeStart, len);
+      const end = arguments[2];
+      const relativeEnd = end === undefined ? len : end >> 0;
+      const final =
+        relativeEnd < 0
+          ? Math.max(len + relativeEnd, 0)
+          : Math.min(relativeEnd, len);
+      while (k < final) {
+        O[k] = value;
+        k++;
+      }
+      return O;
+    },
+  });
+}
 
 const Star = ({
   stars, outOf, full, empty, stroke,
